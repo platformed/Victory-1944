552e5779118db2a4ec5b58f7b0ef2a24e86014d3
Kyler Smith <kylersmith7@gmail.com>
2015-11-21 15:14:37 -0800

    Ammo and first weapon

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,27 +1,42 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Weapon : MonoBehaviour {
+	public Text ammoText;
 	public Transform cam;
 	public float damage;
 	public float delay;
+	public bool continuousFiring;
 
 	public GameObject test;
 
 	bool shooting = false;
 	float currentDelay = 0f;
+	public int ammo;
 	
 	void Update () {
-		if (Input.GetButton("Fire1") && currentDelay <= 0) {
-			shooting = true;
-			currentDelay = delay;
+		if (currentDelay <= 0) {
+			if (continuousFiring) {
+				if (Input.GetButtonDown("Fire1")) {
+					shooting = true;
+					currentDelay = delay;
+				}
+			} else {
+				if (Input.GetButton("Fire1")) {
+					shooting = true;
+					currentDelay = delay;
+				}
+			}
 		}
 	}
 
 	void FixedUpdate() {
+		ammoText.text = ammo.ToString();
+
 		test.SetActive(false);
 
-		if (shooting) {
+		if (shooting && ammo > 0) {
 			shooting = false;
 
 			RaycastHit hit;
@@ -36,6 +51,8 @@ public class Weapon : MonoBehaviour {
 				Debug.DrawRay(transform.position, transform.forward * 50f, Color.red);
 				Debug.Log("HIT " + hit.transform.name);
 			}
+
+			ammo--;
 
 			test.SetActive(true);
 		}
